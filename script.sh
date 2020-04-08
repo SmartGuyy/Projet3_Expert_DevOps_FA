655c5f7b35a5f92c1920f66317c1187bad1fdcdb
Smartguy <franz.alliod@viacesi.fr>
2020-04-08 22:53:20 +0200

    updated script, centos version and instructions

--- script.sh
+++ script.sh
@@ -1,8 +1,38 @@
 #!/bin/bash
- 
-yum update -y 
-yum install -y  yum-utils device-mapper-persistent-data lvm2
-yum install -y  docker
-yum install -y  epel-repo
-yum install -y  ansible
-yum install -y nano
+
+dnf update -y 
+
+# removing previous install of docker
+rm -rf /var/lib/docker
+dnf remove docker docker-ce docker-ce-cli containerd.io
+
+# installing GUI tools
+dnf groupinstall -y gnome-desktop
+dnf install -y xorg*
+
+# removing useless tools
+dnf remove -y initial-setup initial-setup-gui
+
+# set persistent GUI
+systemctl isolate graphical.target
+systemctl set-default graphical.target
+
+# to add docker repository
+dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+
+# to add latest docker-ce version
+dnf install docker-ce --nobest -y
+
+# installing tools
+dnf install -y device-mapper-persistent-data lvm2 nano
+
+# enabling and starting docker
+systemctl start docker
+systemctl enable docker
+
+# installing ansible
+dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm -y
+dnf install -y ansible
+
+# set keyboard to french
+localectl set-keymap fr
